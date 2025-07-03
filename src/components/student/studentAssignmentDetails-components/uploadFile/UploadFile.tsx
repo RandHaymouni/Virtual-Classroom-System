dda2be9c8ba4373fb341466bf3ddf9005efadbbb
Mays <mayssweite@gmail.com>
2025-07-03 23:27:09 +0300

    Added choose files functionality

--- src/components/student/studentAssignmentDetails-components/uploadFile/UploadFile.tsx
+++ src/components/student/studentAssignmentDetails-components/uploadFile/UploadFile.tsx
@@ -1,13 +1,80 @@
 import { LuUpload } from "react-icons/lu";
 import classes from './uploadFile.module.css';
 import { TbFileSearch } from "react-icons/tb";
+import { useRef, useState, type ChangeEvent, type MouseEvent } from "react";
+
 const UploadFile = () => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [selectedFiles, setSelectedFiles] = useState<Array<string> | null>(null);
+
+    const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            ;
+            const filesNames = Array.from(files).map(file => file.name);
+            setSelectedFiles(prevSelection => {
+                if (prevSelection) {
+                    const distinctFiles = filesNames.filter(newFile =>
+                        !prevSelection.includes(newFile)
+                    );
+                    return [...prevSelection, ...distinctFiles];
+                }
+                else return filesNames;
+            });
+            console.log('Selected files:', selectedFiles);
+            event.target.value = '';
+        } else {
+            setSelectedFiles(null);
+        }
+    };
+
+    const removeFile = (index: number) => {
+        if (selectedFiles) {
+            const updatedFiles = [...selectedFiles];
+            updatedFiles.splice(index, 1);
+            setSelectedFiles(updatedFiles.length > 0 ? updatedFiles : null);
+        }
+    }
+
+    //  ensure size + activate drag and drop functionality
     return (
         <div className={classes.uploadFileContainer}>
             <LuUpload className={classes.uploadIcon} /><h5 className={classes.uploadStatement}> Upload your files </h5>
             <h6 >Drag and drop your files here or click to browse</h6>
-            <button className={classes.uploadBtn}><TbFileSearch />       Choose Files</button>
+            <input
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+                accept=".pdf,.docx,.zip,.jpg"
+                multiple
+            />
+            <button className={classes.uploadBtn} onClick={handleButtonClick}><TbFileSearch /> Choose Files</button>
             <h6 className={classes.formats}>Supported formats: PDF, DOCX, ZIP, JPG (Max 10MB)</h6>
+
+            {selectedFiles != null && <div className={classes.selectedFile}>
+                <h6>Selected Files:</h6>
+                <ul>
+                    {selectedFiles.map((fileName, index) => (
+                        <li key={index} className={classes.fileItem}>
+                            <h3 className={classes.fileName}>{fileName}</h3>
+                            <button
+                                onClick={() => removeFile(index)}
+                                className={classes.removeBtn}
+                                type="button"
+                            >
+                                ×
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>}
         </div>
     )
 }
--- src/pages/student/studentAssignmentDetails/StudentAssignmentDetails.tsx
+++ src/pages/student/studentAssignmentDetails/StudentAssignmentDetails.tsx
@@ -1,19 +1,30 @@
+import CancelSubmit from '../../../components/student/studentAssignmentDetails-components/cancelSubmit/CancelSubmit';
+import CommentSection from '../../../components/student/studentAssignmentDetails-components/commentSection/CommentSection';
 import AssignmentHeader from '../../../components/student/studentAssignmentDetails-components/header/AssignmentHeader'
-
+import classes from '../../../components/student/studentAssignmentDetails-components/header/assignmentHeader.module.css'
+import UploadFile from '../../../components/student/studentAssignmentDetails-components/uploadFile/UploadFile';
+import UpperHeader from '../../../components/student/studentAssignmentDetails-components/upperHeader/UpperHeader';
 const StudentAssignmentDetails = () => {
   return (
     <>
-      < AssignmentHeader assignmentStatus= "Graded"
-      title= "React Component Development Assignment"
-      points={100}
-      dueDate= "July 15, 2025"
-      description= "Create a responsive React component using TypeScript and CSS modules. The component should handle user interactions and display assignment status dynamically."
-     onBackClick={() => {
-        console.log("Navigate back to assignments list");
-      }}
+      <UpperHeader />
+      <div className={classes.container}>
+        < AssignmentHeader assignmentStatus="Graded"
+          title="React Component Development Assignment"
+          points={100}
+          dueDate="July 15, 2025"
+          description="Create a responsive React component using TypeScript and CSS modules. The component should handle user interactions and display assignment status dynamically."
+          onBackClick={() => {
+            console.log("Navigate back to assignments list");
+          }} />
 
-      />
+        <UploadFile />
+        <CommentSection />
+        <CancelSubmit />
+      </div>
+
     </>
+
   )
 }
 
