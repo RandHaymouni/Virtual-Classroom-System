ebf0de91037c8ca67d3b7f4a6e69254d2635c73b
Mays <mayssweite@gmail.com>
2025-07-15 22:25:39 +0300

    Modified assignment status

--- src/components/teacher/teacherAssignmentDetails-components/upperHeader/UpperHeader.tsx
+++ src/components/teacher/teacherAssignmentDetails-components/upperHeader/UpperHeader.tsx
@@ -1,21 +1,25 @@
 import classes from './upperHeader.module.css';
 import { IoMdArrowRoundBack } from "react-icons/io";
-const assignmentStatus = "Submitted"; 
 
-const UpperHeader = () => {
+const UpperHeader = ({ dueDate }: { dueDate: string }) => {
+    const calcDays = (dueDate: string): number => {
+        const due = new Date(dueDate);
+        const today = new Date();
+        const timeDiff = due.getTime() - today.getTime();
+        const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+        console.log(`Days until due: ${daysDiff}`);
+        return Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Convert to days
+    };
+
+    const isActive: string = calcDays(dueDate) >= 0 ? 'Active' : 'Overdue';
     return (
         <div className={classes.upperHeader}>
             <button className={classes.backBtn} onClick={() => window.location.href = '/TeacherViewClass'}>
                 <IoMdArrowRoundBack /> Back to Assignments
             </button>
-            <h5 className={`${classes.status} ${assignmentStatus === "Submitted" ?
-                classes.statusSubmitted :
-                (assignmentStatus === "Not Submitted" ?
-                    classes.statusNotSub :
-                    classes.statusGraded
-                )}
-      `}>
-                {assignmentStatus}</h5>
+            <h5 className={`${classes.status} ${isActive === "Active" ?
+                classes.active : classes.notActive}`}>
+                {isActive}</h5>
         </div>
     )
 }
